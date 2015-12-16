14aeda33f66ef5c144e9d8b9e26177a9c533dc12
Quinntyne Brown <Quinntyne.Brown@shawmedia.ca>
2015-12-16 12:42:29 -0500

    basic routing

---
+++ src/components/about/aboutPage.js
@@ -0,0 +1,18 @@
+﻿"use strict";
+
+var React = require('react');
+
+var About = React.createClass({
+    render: function () {
+        return (
+            <div>
+                <h1>About</h1>
+                <p>
+                    This is the about page!
+                </p>
+            </div>
+        );
+    }
+});
+
+module.exports = About;
