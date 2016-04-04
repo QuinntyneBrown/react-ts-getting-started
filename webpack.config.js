c2a815be4c39d99246bf0d4ce7dee6b2bb5109d5
Quinntyne Brown <Quinntyne.Brown@shawmedia.ca>
2016-04-04 17:05:31 -0400

    updates

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+﻿module.exports = {
+  entry: './wwwroot/app.tsx',
+  output: {
+    filename: "./dist/bundle.js"
+  },
+  resolve: {
+    extensions:['','.webpack.js','.web,js','.ts','.tsx','.js']
+  },
+  module: {
+    loaders: [
+        {
+          test: /\.tsx$/, loader: "ts-loader"
+        },
+        {
+          test: /\.css$/, exclude: [/node_modules/], loader: "style-loader!css-loader"
+        }
+    ]
+  }
+
+}
