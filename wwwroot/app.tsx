3ecf4f8424190e4f343ab3897fc26d6364480405
Quinntyne Brown <quinntynebrown@gmail.com>
2016-04-04 20:43:21 -0400

    app component

--- wwwroot/app.tsx
+++ wwwroot/app.tsx
@@ -1,3 +1,17 @@
 ﻿import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+interface Props {
+
+}
+
+interface State {
+
+}
+
+export class AppComponent extends React.Component<Props, State> {
+    render () { return(<div><h1>App Component</h1></div >) }
+}
+
+ReactDOM.render(<AppComponent/>,document.getElementById('root'))
+
