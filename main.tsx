942bc71fe79b133e84694eeff92abe7c42a547e5
Quinntyne Brown <quinntynebrown@gmail.com>
2016-08-09 18:06:20 -0400

    update

--- main.tsx
+++ main.tsx
@@ -1,5 +1,5 @@
 ﻿import * as React from "react";
 import * as ReactDOM from "react-dom";
-import AppComponent from "./wwwroot/app";
+import AppComponent from "./src/components/app";
 
 ReactDOM.render(<AppComponent/>, document.getElementById('root'))
